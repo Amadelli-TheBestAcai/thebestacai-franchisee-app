bfa8ec4b44766af4f59cbf8e3ac367c28337b8f6
MaysharaF <mayshara@alunos.utfpr.edu.br>
2020-09-22 22:39:13 -0300

    Adicionando icones

---
+++ src/styles/Icons.ts
@@ -0,0 +1,6 @@
+export { PointOfSale } from '@styled-icons/material/PointOfSale'
+export { ListOl } from '@styled-icons/boxicons-regular/ListOl'
+export { Motorcycle } from '@styled-icons/fa-solid/Motorcycle'
+export { ArrowLeftRight } from '@styled-icons/bootstrap/ArrowLeftRight'
+export { BarChart } from '@styled-icons/ionicons-sharp/BarChart'
+export { Cashapp } from '@styled-icons/simple-icons/Cashapp'
