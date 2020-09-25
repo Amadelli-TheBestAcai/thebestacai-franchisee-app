743509d3308d417eba5233a6171b0fb7b2051998
Wirlley Delfino <wirlley_d@hotmail.com>
2020-09-25 18:03:24 -0300

    Criando container e componente de items

---
+++ src/components/Item/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { Item } from '../../models/saleItem'
+
+// import { Container } from './styles';
+
+type IProps = {
+  item: Item
+}
+
+const Item: React.FC<IProps> = ({ item }) => {
+  return <div />
+}
+
+export default Item
---
+++ src/containers/Items/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { Item } from '../../models/saleItem'
+
+// import { Container } from './styles';
+
+type IProps = {
+  items: Item[]
+}
+
+const Items: React.FC<IProps> = ({ items }) => {
+  return <div />
+}
+
+export default Items
