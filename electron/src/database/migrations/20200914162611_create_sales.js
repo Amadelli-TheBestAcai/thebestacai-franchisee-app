ace4db3828b526e2f9d9c23cd03356cdc6971480
Wirlley Delfino <wirlley_d@hotmail.com>
2020-09-29 14:58:35 -0300

    Gerando nova coluna de nome em sales

--- electron/src/database/migrations/20200914162611_create_sales.js
+++ electron/src/database/migrations/20200914162611_create_sales.js
@@ -2,6 +2,7 @@ exports.up = async (knex) =>
   knex.schema.createTable('sales', (table) => {
     table.string('id')
     table.decimal('change_amount', 8, 2)
+    table.string('name')
     table.string('type')
     table.decimal('discount', 8, 2)
     table.boolean('to_integrate')
--- electron/src/models/Sale.ts
+++ electron/src/models/Sale.ts
@@ -1,6 +1,7 @@
 export interface Sale {
   id: string
   change_amount: number
+  name: string
   type: string
   discount: number
   to_integrate: boolean
