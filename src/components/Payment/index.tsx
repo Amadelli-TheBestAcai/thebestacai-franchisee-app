043a286dc673093a18835ab193db3b55530e1b63
Wirlley Delfino <wirlley_d@hotmail.com>
2020-09-29 10:01:06 -0300

    Ajustando sidebar

--- src/components/Payment/index.tsx
+++ src/components/Payment/index.tsx
@@ -18,7 +18,7 @@ const Payment: React.FC<IProps> = ({ payment, removePayment }) => {
         <Description>{PaymentType[type]}</Description>
       </Column>
       <Column span={10}>
-        <Description>R$ {amount?.toFixed(2)}</Description>
+        <Description>R$ {amount}</Description>
       </Column>
       <Column span={4}>
         <Button onClick={() => removePayment(payment)}>
