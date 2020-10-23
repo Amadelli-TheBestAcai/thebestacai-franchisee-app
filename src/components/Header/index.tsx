3946104369066d39867371b3d5150b495c316ee7
MaysharaF <mayshara@alunos.utfpr.edu.br>
2020-10-23 00:16:05 -0300

    alterando header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
-import { Container, Logo } from './styles'
-import ImageLogo from '../../assets/img/logo.png'
+import { Container, LogosConatiner, Logo1, Logo2 } from './styles'
+import LogoName from '../../assets/img/logo_name.png'
+import Logo from '../../assets/img/logo_amadelli.png'
 const Header: React.FC = () => {
   return (
     <Container>
-      <Logo src={ImageLogo} />
-      {/* Apenas deixei a estilização para adicionar a imagem para logo */}
+      
+      <LogosConatiner>
+         <Logo1 src={LogoName} />
+         <Logo2 src={Logo} />
+      </LogosConatiner>
+
+
+
     </Container>
   )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,10 +1,38 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const Container = styled.div`
 
-export const Logo = styled.img`
-  position: absolute;
-  width: 50vw;
-  height: 10vh;
-  margin: 10px 0 10px 50px;
 `
+
+export const LogosConatiner = styled.div`
+  position: absolute;
+  width: 70vw;
+  height: 10vh;
+  margin: 10px 0 10px 68px;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+export const Logo1 = styled.img`
+  width: 27vw;
+  height: 7vh;
+
+`
+
+export const Logo2 = styled.img`
+  width: 10vw;
+  height: 8vh;
+
+`
+
+
+// export const Divisor = styled.div`
+// position: absolute;
+// width: 50px;
+// top: 37px;
+
+// border: 1px solid #CECECE;
+// transform: rotate(90deg);
+// `
