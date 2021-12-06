1553384d8a54ac11bb34bcca08e762d91047ce9f
Wirlley Delfino <wirlley_d@hotmail.com>
2021-12-06 12:23:01 -0300

    Adicionando env-config

---
+++ env-config.js
@@ -0,0 +1,11 @@
+const env_dev = {
+  API_LOG: 'https://api-logs.herokuapp.com',
+  NFCe_AMBIENTE: 2,
+  NFCe_Token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC8xNS4yMjguMTAuNTFcL2FwaVwvbG9naW4iLCJpYXQiOjE2Mjg4NzI5NTMsIm5iZiI6MTYyODg3Mjk1MywianRpIjoialdIbkZuNXJOMEVrcDc3TCIsInN1YiI6OTk5OTksInBydiI6ImY0N2YyODQzZjI0ZTFiMTcwNDEzM2FiNTBhMWQzMzZiZmE0MDVmYTEifQ.ehP2khRzZCIj19IqL-IQmVFeSjaEQ7XQA_-7BulPnLk',
+  API_DASH: 'https://hml-thebestacai-api.herokuapp.com',
+  API_AUTH: 'https://hml-authenticator-api.herokuapp.com',
+  API_SALES_HANDLER: 'https://hml-sales-handler-api.herokuapp.com',
+  CHAT_DASH: 'https://hml-thebestacai-chat.herokuapp.com'
+}
+
+export default env_dev
